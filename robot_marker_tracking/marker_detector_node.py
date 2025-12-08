b3b48b1f68a8041ea66f364b9a96bceb24474c42
danyatennn <tendaniil04@gmail.com>
2025-12-08 22:34:39 +0000

    calibration coord

---
+++ robot_marker_tracking/marker_detector_node.py
@@ -0,0 +1,51 @@
+from rclpy.node import Node
+
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+
+import cv2
+import numpy as np
+
+class MarkerDetectorNode(Node):
+    def __init__(self):
+        super().__init__('marker_detector_node')
+        self.bridge = CvBridge()
+        self.subscription = self.create_subscription(
+            Image,
+            '/camera/image_raw',
+            self.image_callback,
+            10
+        )
+
+        #find right parameters
+        self.margin_of_error = 50
+        self.lower_bound = np.array([0, 120, 70])
+        self.upper_bound = np.array([10, 255, 255])
+        self.get_logger().info('MarkerDetectorNode started') 
+
+
+    def image_callback(self, msg: Image):
+        frame = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8') # OpenCV uses BGR, ROS does not, we need to convert
+        h, w, _ = frame.shape
+        self.get_logger().info(f"Picture size: {w}x{h}")
+        hsv_image = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV) # convert picture BGR → HSV
+
+        mask = cv2.inRange(hsv_image, self.lower_bound, self.upper_bound)
+        contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) #RETR_EXTERNAL for external contours
+        if not contours:
+            self.get_logger().info("No marker was found")
+            return
+        
+        marker = max(contours, key=cv2.contourArea)
+        marker_area = cv2.contourArea(marker)
+        if marker_area < self.margin_of_error:
+            self.get_logger().info("Noise")
+            return
+        
+        M = cv2.moments(marker)
+        if M["m00"] == 0:
+            return
+        
+        center_x = int(M["m10"] / M["m00"])
+        center_y = int(M["m01"] / M["m00"])
+        self.get_logger().info(f"Marker coordinates: x={center_x}, y={center_y}, area={marker_area:.2f}")
