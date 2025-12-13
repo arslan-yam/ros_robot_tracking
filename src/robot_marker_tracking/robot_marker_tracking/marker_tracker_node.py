5ddc33fba7097f65017263f3487dfa5087248759
armaiamaletdinov <armaiamaletdinov@edu.hse.ru>
2025-12-13 14:24:10 +0000

    rviz is supported

--- src/robot_marker_tracking/robot_marker_tracking/marker_tracker_node.py
+++ src/robot_marker_tracking/robot_marker_tracking/marker_tracker_node.py
@@ -4,6 +4,8 @@ from rclpy.qos import qos_profile_sensor_data
 
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
+from geometry_msgs.msg import PoseStamped
+from nav_msgs.msg import Path
 
 import cv2
 import numpy as np
@@ -11,6 +13,10 @@ import numpy as np
 class MarkerTrackerNode(Node):
     def __init__(self):
         super().__init__('marker_tracker_node')
+        self.publisher = self.create_publisher(Path, '/robot_path', 10)  # publish path
+        self.timer = self.create_timer(0.1, self.publish_path)  # publish every 0.1 sec
+        self.path = Path()  # create path object for rviz
+        self.path.header.frame_id = "map"
         self.bridge = CvBridge()
         self.subscription = self.create_subscription(
             Image,
@@ -44,15 +50,19 @@ class MarkerTrackerNode(Node):
         Y = float(A[1, 0] * u + A[1, 1] * v + A[1, 2])
         return X, Y
     
+    def publish_path(self):
+        self.publisher.publish(self.path)
+        self.get_logger().info(f"Publishing path with {len(self.path.poses)} poses")
+
     def image_callback(self, msg: Image):
         self.get_logger().info("Got image in callback")
         frame = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8') # OpenCV uses BGR, ROS does not, we need to convert
         h, w, _ = frame.shape
         self.get_logger().info(f"Picture size: {w}x{h}")
-        hsv_image = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV) # convert picture BGR → HSV
+        hsv_image = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)  # convert picture BGR → HSV
 
         mask = cv2.inRange(hsv_image, self.lower_bound, self.upper_bound)
-        contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) #RETR_EXTERNAL for external contours
+        contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)  #RETR_EXTERNAL for external contours
         if not contours:
             self.get_logger().info("No marker was found")
             return
@@ -70,6 +80,18 @@ class MarkerTrackerNode(Node):
         center_x = int(M["m10"] / M["m00"])
         center_y = int(M["m01"] / M["m00"])
         X, Y = self.pixel_to_world(center_x, center_y)
+
+        # create pose for current path
+        pose = PoseStamped()
+        pose.header.stamp = self.get_clock().now().to_msg()
+        pose.header.frame_id = "map"
+        pose.pose.position.x = X
+        pose.pose.position.y = Y
+        pose.pose.position.z = 0.0  # we flatten
+        self.path.poses.append(pose)
+        if len(self.path.poses) > 100000:  # last 100000 poses
+            self.path.poses.pop(0)
+
         self.get_logger().info(
             f"Pixel(u,v)=({center_x:.1f},{center_y:.1f}) -> World(X,Y)=({X:.3f},{Y:.3f})"
         )
